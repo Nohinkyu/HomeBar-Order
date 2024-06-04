fc6312a55630e25bff879819ae9d11742ef071ed
Nohinkyu <wjd2256@naver.com>
2024-06-04 21:01:19 +0900

    feat: SignInViewModel 에서 로그인 유무 확인하는 함수 구현

--- app/src/main/java/com/devik/homebarorder/ui/screen/signin/SignInViewModel.kt
+++ app/src/main/java/com/devik/homebarorder/ui/screen/signin/SignInViewModel.kt
@@ -3,11 +3,15 @@ package com.devik.homebarorder.ui.screen.signin
 import androidx.lifecycle.ViewModel
 import com.devik.homebarorder.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(private val userRepository: UserRepository) :
     ViewModel() {
+
+    private val _isSignIn = MutableStateFlow<Boolean>(false)
+    val isSignIn = _isSignIn
 
     fun saveUserImageUrl(userImageUrl: String) {
         userRepository.saveUserImageUrl(userImageUrl)
@@ -15,5 +19,13 @@ class SignInViewModel @Inject constructor(private val userRepository: UserReposi
 
     fun saveUserMailAddress(mailAddress: String) {
         userRepository.saveUserMailAddress(mailAddress)
+    }
+
+    fun checkUserInfo() {
+        if (userRepository.getUserMailAddress().isNotBlank() && userRepository.getUserMailAddress()
+                .isNotBlank()
+        ) {
+            _isSignIn.value = true
+        }
     }
 }
