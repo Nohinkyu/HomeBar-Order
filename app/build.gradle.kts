85730d3fdff1bb59a886912dbc0293222d095a4c
Nohinkyu <wjd2256@naver.com>
2024-06-04 20:11:22 +0900

    chore: Navigation 라이브러리 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,4 +99,7 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // Navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
 }
