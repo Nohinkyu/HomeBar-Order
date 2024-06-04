abcce0ecba1715bb95bc7d4c596be385e2b67f09
Nohinkyu <wjd2256@naver.com>
2024-06-04 20:12:23 +0900

    feat: UserLocalDataSource Constants 값 수정

--- app/src/main/java/com/devik/homebarorder/data/source/local/UserLocalDataSource.kt
+++ app/src/main/java/com/devik/homebarorder/data/source/local/UserLocalDataSource.kt
@@ -1,25 +1,24 @@
 package com.devik.homebarorder.data.source.local
 
-import com.devik.homebarorder.util.Constants.KEY_MAIL_ADDRESS
-import com.devik.homebarorder.util.Constants.KEY_PROFILE_IMAGE
+import com.devik.homebarorder.util.Constants
 import javax.inject.Inject
 
 class UserLocalDataSource @Inject constructor(private val preferenceManager: PreferenceManager) :
     UserDataSource {
 
     override fun saveUserMail(mailAddress: String) {
-        preferenceManager.putString(KEY_MAIL_ADDRESS, mailAddress)
+        preferenceManager.putString(Constants.KEY_MAIL_ADDRESS, mailAddress)
     }
 
     override fun getUserMail(): String {
-        return preferenceManager.getString(KEY_MAIL_ADDRESS, "")
+        return preferenceManager.getString(Constants.KEY_MAIL_ADDRESS, "")
     }
 
     override fun saveUserProfileImage(imageUrl: String) {
-        preferenceManager.putString(KEY_PROFILE_IMAGE, imageUrl)
+        preferenceManager.putString(Constants.KEY_PROFILE_IMAGE, imageUrl)
     }
 
     override fun getUserProfileImage(): String {
-        return preferenceManager.getString(KEY_PROFILE_IMAGE, "")
+        return preferenceManager.getString(Constants.KEY_PROFILE_IMAGE, "")
     }
 }
