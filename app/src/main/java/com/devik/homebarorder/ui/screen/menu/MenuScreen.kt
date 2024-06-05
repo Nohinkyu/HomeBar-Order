73ed98f65b76141dbbff4c99921688bd1ac46ea5
Nohinkyu <wjd2256@naver.com>
2024-06-06 06:28:07 +0900

    feat: 첫 로그인 유무 확인해서 메뉴얼 다이얼로그 출력하는 로직 구현

--- app/src/main/java/com/devik/homebarorder/ui/screen/menu/MenuScreen.kt
+++ app/src/main/java/com/devik/homebarorder/ui/screen/menu/MenuScreen.kt
@@ -6,32 +6,48 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.devik.homebarorder.R
 import com.devik.homebarorder.ui.component.topappbar.MenuIconWithTitleAppBar
+import com.devik.homebarorder.ui.dialog.ManualDialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MenuScreen(navController: NavController) {
 
-    val context = LocalContext.current
-    val viewModel: MenuScreenViewModel = hiltViewModel()
+    CompositionLocalProvider(
+        androidx.lifecycle.compose.LocalLifecycleOwner provides androidx.compose.ui.platform.LocalLifecycleOwner.current,
+    ) {
+        val context = LocalContext.current
+        val viewModel: MenuScreenViewModel = hiltViewModel()
+        val manualDialogState by viewModel.manualDialogState.collectAsStateWithLifecycle()
+        viewModel.checkIsFirstSignIn()
 
-    Scaffold(
-        topBar = { MenuIconWithTitleAppBar(context.getString(R.string.app_name)) },
-        modifier = Modifier.padding(top = 8.dp)
-    ) { paddingValues ->
+        if (manualDialogState) {
+            ManualDialog() {
+                viewModel.manualDialogDismissRequest()
+            }
+        }
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
+        Scaffold(
+            topBar = { MenuIconWithTitleAppBar(context.getString(R.string.app_name)) },
+            modifier = Modifier.padding(top = 8.dp)
+        ) { paddingValues ->
 
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+
+            }
         }
     }
 }
--- app/src/main/java/com/devik/homebarorder/ui/screen/menu/MenuScreenViewModel.kt
+++ app/src/main/java/com/devik/homebarorder/ui/screen/menu/MenuScreenViewModel.kt
@@ -1,11 +1,31 @@
 package com.devik.homebarorder.ui.screen.menu
 
 import androidx.lifecycle.ViewModel
+import com.devik.homebarorder.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class MenuScreenViewModel @Inject constructor() :
+class MenuScreenViewModel @Inject constructor(private val userRepository: UserRepository) :
     ViewModel() {
 
+    private val _isFirstSignIn = MutableStateFlow<Boolean>(userRepository.getIsFirstSignIn())
+    val isFirstSignIn: StateFlow<Boolean> = _isFirstSignIn
+
+    private val _manualDialogState = MutableStateFlow<Boolean>(false)
+    val manualDialogState:StateFlow<Boolean> = _manualDialogState
+
+    fun checkIsFirstSignIn() {
+        if(_isFirstSignIn.value)  {
+            _manualDialogState.value = true
+            _isFirstSignIn.value = false
+            userRepository.saveIsFirstSignIn()
+        }
+    }
+
+    fun manualDialogDismissRequest() {
+        _manualDialogState.value = false
+    }
 }
