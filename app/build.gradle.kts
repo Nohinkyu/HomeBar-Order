f401c3a04efcf4815af95ccf5e9a091e2664bd2f
Nohinkyu <wjd2256@naver.com>
2024-06-06 06:27:04 +0900

    chore: compose lifecycle 라이브러리 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.1")
 
 
     // Google Sign in
