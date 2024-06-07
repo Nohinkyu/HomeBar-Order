5649915d08562e57e39e0f26c583a2999c14d374
Nohinkyu <wjd2256@naver.com>
2024-06-07 14:16:00 +0900

    feat: BackIconWithTitleAppBar 구현

---
+++ app/src/main/java/com/devik/homebarorder/ui/component/topappbar/BackIconWithTitleAppBar.kt
@@ -0,0 +1,28 @@
+package com.devik.homebarorder.ui.component.topappbar
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BackIconWithTitleAppBar(title: String) {
+    TopAppBar(
+        title = {
+            Text(text = title, fontSize = 24.sp)
+        },
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack, contentDescription = "back_button"
+                )
+            }
+        }
+    )
+}
